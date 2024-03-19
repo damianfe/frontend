e0c651d9944181b82c31e12d116c287908065452
damianfe <damianrocio12@gmail.com>
2024-03-19 11:54:51 -0300

    feat: update structure footer done

---
+++ src/lib/data.ts
@@ -0,0 +1,55 @@
+export const links = [
+    {
+        id:"home",
+        name:"Home",
+        hash:  "#home"
+    },
+    {
+        id:"features",
+        name:"Features",
+        hash:  "#features"
+    },
+    {
+        id:"coaches",
+        name:"Coaches",
+        hash:  "#coaches"
+    },
+    {
+        id:"faq",
+        name:"Faq",
+        hash:  "#faq"
+    },
+    {
+        id:"contact",
+        name:"Contact",
+        hash:  "#contact"
+    },
+];
+export const socialMedia = [
+
+    {
+        id:1,
+        link: 'https://www.facebook.com/app.lucky.quit',
+        icon:'/facebook.png',
+        alt:'facebook logo',
+    },
+    {
+        id:2,
+        link: 'https://www.twitter.com/app.lucky.quit',
+        icon:'/x.png',
+        alt:'twitter logo',
+    },
+    {
+        id:3,
+        link: 'https://www.instagram.com/app.lucky.quit',
+        icon:'/instagram.png',
+        alt:'instagram logo',
+    },
+    {
+        id:4,
+        link: 'https://www.threads.net/app.lucky.quit',
+        icon:'/threads.png',
+        alt:'threads logo',
+    },
+
+]
